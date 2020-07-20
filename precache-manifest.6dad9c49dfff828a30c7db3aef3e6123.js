6a60286cb7ac2c65271ddbfa8b744c591f8983ba
Dawid Piwko <dpiwko@gmail.com>
2020-07-20 11:38:32 +0200

    Updates

---
+++ precache-manifest.6dad9c49dfff828a30c7db3aef3e6123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745e915a91b4e14ba3747baa1bb09918",
+    "url": "/contact-book/index.html"
+  },
+  {
+    "revision": "caf16613503e85f7c5ce",
+    "url": "/contact-book/static/css/main.d4836f8c.chunk.css"
+  },
+  {
+    "revision": "eaf4374d59306c9fe346",
+    "url": "/contact-book/static/js/2.7802857b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contact-book/static/js/2.7802857b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caf16613503e85f7c5ce",
+    "url": "/contact-book/static/js/main.6881e2fe.chunk.js"
+  },
+  {
+    "revision": "36cb17b585069998441e",
+    "url": "/contact-book/static/js/runtime-main.b07d51ec.js"
+  }
+]);
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as List } from './List/List';
-export { default as ListItem } from './ListItem/ListItem';
-export { default as Loading } from './Loading/Loading';
-export { default as UsersList } from './UsersList/UsersList';
